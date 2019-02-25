f33774358e3027d8b6e44f31de5ec5be81a1e7d1
mojim <mojim@192.168.1.108>
2019-02-25 11:30:37 +0100

    Primera Historia de Usuario Completada

---
+++ EnZinium/src/main/java/org/lasencinas/enzinium/Address.java
@@ -0,0 +1,59 @@
+
+package org.lasencinas.enzinium;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import org.lasencinas.enzinium.GenSig;
+
+public class Address {
+    
+    //ATRIBUTOS
+    private PublicKey PK;
+    private PrivateKey SK;
+    private double balance = 0d;
+    private String symbol = "EZI";
+    
+    //CONSTRUCTOR
+    public Address(){};
+    
+    
+    //SETTERS & GETTERS
+    public void setPK (PublicKey PK) {
+        this.PK = PK;
+    }
+    
+    public PublicKey getPK() {
+        return this.PK;
+    }
+    
+    public void setSK(PrivateKey SK) {
+        this.SK = SK;
+    }
+    
+    public PrivateKey getSK() {
+        return this.SK;
+    }
+        
+    public double getBalance() {
+        return balance;
+    }
+    
+    public String getSymbol() {
+        return symbol;
+    }
+    
+    
+    //MÉTODOS
+    
+    public void generateKeyPair() {
+        this.PK = GenSig.generateKeyPair().getPublic();
+        this.SK = GenSig.generateKeyPair().getPrivate();
+    }
+    
+    @Override
+    public String toString() {
+        return "\n" + "PK = " + getPK().hashCode() + "\n" +
+                "Balance = " + getBalance() + " " + getSymbol() + "\n";
+    }    
+   
+}
---
+++ EnZinium/src/main/java/org/lasencinas/enzinium/TokenContract.java
@@ -0,0 +1,7 @@
+
+package org.lasencinas.enzinium;
+
+
+public class TokenContract {
+    
+}
